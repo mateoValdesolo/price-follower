4a383656b5827dee360bac97fa71412489634f30
Mateo Valdesolo <mateovalde@gmail.com>
2024-05-14 23:01:41 -0300

    Initial Commit

---
+++ src/test/java/ar/com/pricefollower/PriceFollowerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.pricefollower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceFollowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
